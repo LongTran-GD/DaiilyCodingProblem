ccfebbcc291b9311eb3e0787806d82f99beb3763
THKL <longthk19@gmail.com>
2022-02-17 14:48:39 +0700

    Start

---
+++ C++/Day2/Day2/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int returnEqual(int ArrayFind[], int k, int length) {
+	int ArrPro[]=;
+	int i = 0, j = 0;
+	for (; i < length; i++)
+	{
+		for (; j < length; j++)
+		{
+			if
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
+int main() {
+	int x[9] = { 1,2,3,4,5,6,7,8,9 };
+	int lengtharr = sizeof(x) / sizeof(x[1]);
+	returnEqual(x, 6, lengtharr);
+}
