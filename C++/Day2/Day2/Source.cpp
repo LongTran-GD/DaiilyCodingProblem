dffb0a300d0cd139eb25f403301f42c46fc4664a
THKL <55828127+LongTran-GD@users.noreply.github.com>
2023-06-27 02:07:07 +0700

    Day2
    This problem was asked by Uber.
    
    Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
    
    For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].

--- C++/Day2/Day2/Source.cpp
+++ C++/Day2/Day2/Source.cpp
@@ -1,25 +1,63 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int returnEqual(int ArrayFind[], int k, int length) {
-	int ArrPro[]=;
-	int i = 0, j = 0;
-	for (; i < length; i++)
-	{
-		for (; j < length; j++)
-		{
-			if
-				break;
-			}
-		}
-	}
-	return 0;
+vector<int> product_array_without_division(vector<int> array) {
+    int n = array.size();
+    vector<int> left_products(n,1 );
+    vector<int> right_products(n, 1);
+    // creates a vector of n integers, all initialized to 1
+    for (int i = 1; i < n; i++) {
+        left_products[i] = left_products[i - 1] * array[i - 1];
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        right_products[i] = right_products[i + 1] * array[i + 1];
+    }
+
+    vector<int> output(n);
+    for (int i = 0; i < n; i++) {
+        output[i] = left_products[i] * right_products[i];
+    }
+
+    return output;
+}
+
+
+vector<int> product_array_with_division(vector<int> array) {
+    int n = array.size();
+    vector<int> output(n);
+    int product = 1;
+
+    for (int i = 0; i < n; i++) {
+        output[i] = product;
+        product *= array[i];
+    }
+
+    product = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        output[i] *= product;
+        product *= array[i];
+    }
+
+    return output;
 }
 
 int main() {
-	int x[9] = { 1,2,3,4,5,6,7,8,9 };
-	int lengtharr = sizeof(x) / sizeof(x[1]);
-	returnEqual(x, 6, lengtharr);
+    vector<int> array = { 1, 2, 3, 4, 5 };
+    vector<int> output_without_division = product_array_without_division(array);
+
+    for (int i = 0; i < output_without_division.size(); i++) {
+        cout << output_without_division[i] << " ";
+    }
+    cout << endl;
+
+    vector<int> output_with_division = product_array_with_division(array);
+
+    for (int i = 0; i < output_with_division.size(); i++) {
+        cout << output_with_division[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
